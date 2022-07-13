68ede54b9e07aca2a16f2a0e5d295371e28865eb
Gayhub666 <96622524+Gayhub666@users.noreply.github.com>
2022-07-13 21:58:47 +0800

    开机延迟自启
    until [ $(getprop init.svc.bootanim) = "stopped" ]
    do
    sleep 3
    done
    做出来的，要是不能用再改

---
+++ service.sh
@@ -0,0 +1,26 @@
+until [ $(getprop init.svc.bootanim) = "stopped" ] ; do
+    sleep 5
+done
+
+service_path=`realpath $0`
+module_dir=`dirname ${service_path}`
+data_dir="/data/clash"
+scripts_dir="${data_dir}/scripts"
+Clash_data_dir="/data/clash"
+Clash_run_path="${Clash_data_dir}/run"
+Clash_pid_file="${Clash_run_path}/clash.pid"
+busybox_path="/data/adb/magisk/busybox"
+
+if [ -f ${Clash_pid_file} ] ; then
+    rm -rf ${Clash_pid_file}
+fi
+until [ $(getprop init.svc.bootanim) = "stopped" ]
+do
+sleep 3
+done
+rm -rf ${Clash_run_path}/run.logs
+nohup ${busybox_path} crond -c ${Clash_run_path} > /dev/null 2>&1 &
+
+${scripts_dir}/clash.service -s && ${scripts_dir}/clash.iptables -s
+
+inotifyd ${scripts_dir}/clash.inotify ${module_dir} >> /dev/null &
