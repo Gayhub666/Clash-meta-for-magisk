9390ff9411babadbf8ab6461c7c21a42b28ae4a6
gayhub666 <johndoe@example.com>
2022-07-20 16:10:44 +0800

    合并鸡哥新版upsub脚本

--- clash/scripts/upSub.sh
+++ clash/scripts/upSub.sh
@@ -1,23 +1,24 @@
 #!/system/bin/sh
-#cfm魔2套餐信息修复版 补上了ua
 . /data/clash/clash.config
 scripts=`realpath $0`
 scripts_dir=`dirname ${scripts}`
 subItem=""
+bkPath="/data/clash/clash-dashboard/dist/subs"
 gotoUrl(){
 name="$1"
 url=${2}
 textIndex=1
 #echo ${name}  ${url}
-"$(curl -H 'User-Agent:ClashforWindows/0.19.23' -I  ${url} -o ${scripts_dir}/${1//:/}.txt)"
-hsums=$(cat "${scripts_dir}/${1//:/}.txt" | wc -l)
+echo "$(curl -H 'User-Agent:ClashforWindows/0.19.23' -I ${url})" >${scripts_dir}/${1//:/}
+hsums=$(cat "${scripts_dir}/${1//:/}" | wc -l)
 while (($textIndex<=$hsums)) do
- lin=$(sed -n ${textIndex},${textIndex}p "${scripts_dir}/${1//:/}.txt");
+ lin=$(sed -n ${textIndex},${textIndex}p "${scripts_dir}/${1//:/}");
  result=$(echo ${lin} | grep 'subscription-userinfo:')
  if [ "${result}" != "" ]; then
-   #echo ${lin//subscription-userinfo:/$1}>>"${scripts_dir}/../clash-dashboard/dist/subs"
-  subItem+=${lin//subscription-userinfo:/$1}"\n"
-  rm "${scripts_dir}/${1//:/}.txt"
+  echo ${lin//subscription-userinfo:/$1} >> "${bkPath}.bk"
+  #subItem+=${lin//subscription-userinfo:/$1}"\n"
+  rm -rf "${scripts_dir}/${1//:/}"
+# echo ${subItem}>>""
   break
  fi
  let 'textIndex++';
@@ -25,7 +26,9 @@ done
 }
 UpStart(){
 i=1;
+
 echo [`TZ=Asia/Shanghai date "+%H:%M:%S"`]"info: 更新订阅流量." >> ${CFM_logs_file}
+
 sums=$(cat "${Clash_config_file}" | wc -l)
 #echo $(($sums-1))
 while (($i <= $sums )) do
@@ -39,24 +42,30 @@ while (($i <= $sums )) do
  lin=${lin// /}
  result=$(echo ${lin} | grep 'url:"http')
  if [ "${result}" != "" ]; then
+ {
   #echo ${lin2//:/}
   lin=${lin//"url:"/}
   #echo ${lin//'"'/}"&flag=clash"
  gotoUrl ${lin2} ${lin//'"'/}"&flag=clash"
  # sleep 5
+ }&
  fi
  result=$(echo ${lin} | grep 'proxy-groups:')
  if [ "${result}" != "" ]; then
   #echo ${lin2//:/}
   #lin=${lin//"url:"/}
-  rm /data/clash/clash-dashboard/dist/subs
-echo `TZ=Asia/Shanghai date "+%Y-%m-%d %H:%M:%S"`>>"${scripts_dir}/../clash-dashboard/dist/subs"
-  echo ${subItem}>>"${scripts_dir}/../clash-dashboard/dist/subs"
+  
   break
  # sleep 5
  fi
  let 'i++';
 done;
+wait;
+echo `TZ=Asia/Shanghai date "+%Y-%m-%d %H:%M:%S"`>"${bkPath}";
+cat "${bkPath}.bk" >>"${bkPath}"
+rm "${bkPath}.bk";
+
+echo "解析完成 ${subItem}"
 }
 while getopts ":kfmpusl" signal; do
     case ${signal} in
